2eb63c6b25aacb81b058f6ca47a56756a5d05243
iriskawada <ir.kawada@gmail.com>
2025-05-22 16:53:20 -0700

    adding macros

--- keyboards/boardsource/microdox/keymaps/iriskawada/keymap.c
+++ keyboards/boardsource/microdox/keymaps/iriskawada/keymap.c
@@ -23,6 +23,34 @@ enum layers {
     _LOWER,
 };
 
+enum custom_keycodes {
+    TMUX_RESTORE = SAFE_RANGE,
+    CTRL_B,
+    CTRL_C,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TMUX_RESTORE:
+            if (record->event.pressed) {
+                // Send Ctrl+B then Ctrl+R
+                SEND_STRING(SS_LCTRL("b") SS_LCTRL("r"));
+            }
+            return false;
+        case CTRL_B:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTRL("b"));
+            }
+            return false;
+        case CTRL_C:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTRL("c"));
+            }
+            return false;
+    }
+    return true;
+}
+
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
@@ -31,20 +59,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LSFT,
-                      KC_LCMD, KC_BKSP, LOWER,        RAISE,   KC_SPC,   KC_SLSH
-  ),
-
-  [_RAISE] = LAYOUT_split_3x5_3(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_TAB,  KC_LEFT, XXXXXXX, KC_UP,   KC_RGHT,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-    KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_QUOT,
-                      XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+                      KC_LCMD, KC_BKSP, LOWER,        RAISE,   KC_SPC,  KC_SLSH
   ),
 
   [_LOWER] = LAYOUT_split_3x5_3(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
-    KC_CAPS, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_DQT,
-                      XXXXXXX, XXXXXXX, XXXXXXX,      KC_ENT,  XXXXXXX, KC_DEL
+    KC_ESC,  KC_1,   KC_2,    KC_3,   KC_5,         KC_6,    KC_7,    KC_8,    KC_F12,    KC_0,
+    KC_TAB,  KC_4,   KC_5,    KC_6,   KC_RGHT,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+    KC_LCTL, KC_7,   KC_8,    KC_9,   XXXXXXX,      XXXXXXX, KC_GRV,  KC_PIPE, KC_BSLS, KC_TILD,
+                     KC_0,    KC_DOT, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+  ),
+
+  [_RAISE] = LAYOUT_split_3x5_3(
+    KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR,
+    KC_ESC,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+    TMUX_RESTORE, KC_TILD, CTRL_C,  XXXXXXX, CTRL_B,       KC_UND,  KC_PPLS, KC_LCBR, KC_RCBR,  KC_DQT,
+                      XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX, XXXXXXX, KC_DEL
   )
 };
