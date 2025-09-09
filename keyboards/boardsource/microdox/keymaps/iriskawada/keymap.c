0e849b197e6745a05140c321d78c7d0d39f9ca51
iriskawada <ir.kawada@gmail.com>
2025-09-08 22:37:01 -0700

    added new macros

--- keyboards/boardsource/microdox/keymaps/iriskawada/keymap.c
+++ keyboards/boardsource/microdox/keymaps/iriskawada/keymap.c
@@ -21,12 +21,16 @@ enum layers {
     _QWERTY,
     _RAISE,
     _LOWER,
+    _CNTRL,
 };
 
 enum custom_keycodes {
     TMUX_RESTORE = SAFE_RANGE,
     CTRL_B,
+    CTRL_V,
     CTRL_C,
+    SCRNSHT,
+    TVNC_EXIT_FS,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -34,45 +38,67 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case TMUX_RESTORE:
             if (record->event.pressed) {
                 // Send Ctrl+B then Ctrl+R
-                SEND_STRING(SS_LCTRL("b") SS_LCTRL("r"));
+                SEND_STRING(SS_LCTL("b") SS_LCTL("r"));
             }
             return false;
         case CTRL_B:
             if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("b"));
+                SEND_STRING(SS_LCTL("b"));
+            }
+            return false;
+        case CTRL_V:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("v"));
             }
             return false;
         case CTRL_C:
             if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("c"));
+                SEND_STRING(SS_LCTL("c"));
             }
             return false;
+        case SCRNSHT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD(SS_LSFT("4")));   // ⌘ down
+            }
+            return false;
+        case TVNC_EXIT_FS:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTRL(SS_LSFT(SS_LALT("f"))));
+            }
     }
     return true;
 }
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define CNTRL MO(_CNTRL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x5_3(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LSFT,
-                      KC_LCMD, KC_BKSP, LOWER,        RAISE,   KC_SPC,  KC_SLSH
+                      KC_LCMD, KC_BSPC, LOWER,        RAISE,   KC_SPC,  KC_SLSH
   ),
 
   [_LOWER] = LAYOUT_split_3x5_3(
-    KC_ESC,  KC_1,   KC_2,    KC_3,   KC_5,         KC_6,    KC_7,    KC_8,    KC_F12,    KC_0,
-    KC_TAB,  KC_4,   KC_5,    KC_6,   KC_RGHT,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-    KC_LCTL, KC_7,   KC_8,    KC_9,   XXXXXXX,      XXXXXXX, KC_GRV,  KC_PIPE, KC_BSLS, KC_TILD,
-                     KC_0,    KC_DOT, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+    KC_ESC,  KC_1,   KC_2,    KC_3,   XXXXXXX,         KC_6,    KC_7,    KC_8,    KC_F12,    KC_0,
+    KC_TAB,  KC_4,   KC_5,    KC_6,   KC_LCMD,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+    KC_LCTL, KC_7,   KC_8,    KC_9,   KC_LSFT,      XXXXXXX, KC_GRV,  KC_PIPE, KC_BSLS, KC_TILD,
+                     KC_0,    KC_DOT, XXXXXXX,      CNTRL,   XXXXXXX, KC_LSFT
   ),
 
   [_RAISE] = LAYOUT_split_3x5_3(
-    KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR,
-    KC_ESC,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-    TMUX_RESTORE, KC_TILD, CTRL_C,  XXXXXXX, CTRL_B,       KC_UND,  KC_PPLS, KC_LCBR, KC_RCBR,  KC_DQT,
-                      XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX, XXXXXXX, KC_DEL
-  )
+    KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC,        KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR,
+    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS,        KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+    TMUX_RESTORE, KC_TILD, CTRL_C,  CTRL_V,  CTRL_B,       LSFT(KC_MINS),  KC_PPLS, KC_LCBR, KC_RCBR,  KC_DQT,
+                      XXXXXXX, XXXXXXX,      CNTRL,      XXXXXXX,        XXXXXXX, KC_DEL
+  ),
+
+  [_CNTRL] = LAYOUT_split_3x5_3(
+    TVNC_EXIT_FS,      XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,      XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, SCRNSHT,      XXXXXXX,        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+    TMUX_RESTORE, XXXXXXX, CTRL_C,  CTRL_V, CTRL_B,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+                     XXXXXXX, XXXXXXX,       XXXXXXX,      XXXXXXX,        XXXXXXX, XXXXXXX
+  ),
 };
